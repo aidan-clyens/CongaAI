91be3925c55cdfcc6fe7d342ffadecf59d92a051
Aidan Clyens <aidanc@rogers.com>
2020-06-16 19:16:22 -0400

    Create empty pygame window

--- conga.py
+++ conga.py
@@ -1,5 +1,26 @@
+import pygame
+
+
+display = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Conga")
+clock = pygame.time.Clock()
+
+
+def run():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
+        clock.tick(60)
+
+
 def main():
-    pass
+    pygame.init()
+    run()
+    pygame.quit()
 
 
 if __name__ == '__main__':
