57d798c29200353d3f0797a6fb22557edb55b621
Aidan Clyens <aidanc@rogers.com>
2020-06-18 15:43:28 -0400

    Select different types of players

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from conga import Game
+import sys
 
 
-def main():
+def main(args):
+    if len(args) < 2:
+        print("Usage: python main.py 'player 1 type' 'player 2 type'")
+        print("Player type: human, random, ai")
+        exit()
+
+    assert args[0] in ["human", "random", "ai"]
+    assert args[1] in ["human", "random", "ai"]
+
     game = Game()
-    game.set_player_1("ai")
-    game.set_player_2("random")
+    game.set_player_1(args[0])
+    game.set_player_2(args[1])
 
     game.run()
 
@@ -12,4 +21,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
